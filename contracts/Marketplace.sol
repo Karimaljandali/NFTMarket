1e9233e8c3aaa949af3b05fbbdd4f87e5ec426e3
Karim Aljandali <karimaljandali@gmail.com>
2022-03-30 16:53:39 -0400

    first commit

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.12;
+
+
+contract Marketplace {
+
+    // Admin Functions, Events, and Structs
+    // ====================================
+    // 1. Set Marketplace Fee
+    // 2. Blacklist NFT Collection
+    // 3. Blacklist NFT Token
+    // 4. Pause&Unpause Contract
+    // 5. Initialization/Constructor
+
+    // Contract Creator Functions, Events, and Structs
+    // ===============================================
+    // 1. Update Fee
+    // 2. Update Fee Recipient Address
+
+
+    // User Functions, Events, and Structs
+    // ===================================
+    // 1. Create Listing
+    // 2. Cancel Listing
+    // 3. Buy Item
+    // 4. Update Listing
+}
